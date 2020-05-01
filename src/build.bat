ff1c682f60a6dccd020283dd66032aaf2e740097
Anand-S23 <anandsroopsingh23@gmail.com>
2020-05-01 19:11:23 -0400

    Create Window

--- src/build.bat
+++ src/build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -Zi ..\chess\src\win32_chess.cpp user32.lib
+cl -Zi ..\chess\src\win32_chess.cpp user32.lib Gdi32.lib
 popd
---
+++ src/win32_chess.cpp
@@ -0,0 +1,108 @@
+#include <Windows.h>
+
+LRESULT CALLBACK
+MainWindowCallback(HWND Window,
+                   UINT Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+    LRESULT Result = 0;
+    
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE");
+        } break;
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY");
+        } break;
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE");
+        } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP");
+        } break;
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int x = Paint.rcPaint.left;
+            int y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            PatBlt(DeviceContext, x, y, Width, Height, WHITENESS);
+            EndPaint(Window, &Paint);
+        } break;
+        default:
+        {
+            Result = DefWindowProcA(Window, Message, WParam, LParam);
+        } break;
+    }
+
+    return Result;
+}
+            
+int CALLBACK
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CmdLine,
+        int CmdShow)
+{
+    WNDCLASS WindowClass = {};
+
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = Instance;
+    //WindowClass.hIcon;
+    WindowClass.lpszClassName = "Chess";
+
+    if (RegisterClassA(&WindowClass))
+    {
+       HWND WindowHandle =
+           CreateWindowExA(
+               0,
+               WindowClass.lpszClassName,
+               "Chess",
+               WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+               CW_USEDEFAULT,
+               CW_USEDEFAULT,
+               CW_USEDEFAULT,
+               CW_USEDEFAULT,
+               0,
+               0,
+               Instance,
+               0);
+
+       if (WindowHandle)
+       {
+           for (;;)
+           {
+               MSG Message;
+               BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+               if (MessageResult > 0)
+               {
+                   TranslateMessage(&Message);
+                   DispatchMessage(&Message);
+               }
+               else
+               {
+                   break;
+               }
+           }
+       }
+       else
+       {
+           // TODO: Logging
+       }
+    }
+    else
+    {
+        // TODO: Logging
+    }
+
+    return 0;
+}
